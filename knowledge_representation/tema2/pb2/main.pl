9ed0b0e4ae22ccac0de4013e522928eacc72f7a2
Mihail Feraru <mihailferaru2000@gmail.com>
2023-02-05 20:14:06 +0200

    done pb2

--- knowledge_representation/tema2/pb2/main.pl
+++ knowledge_representation/tema2/pb2/main.pl
@@ -1,18 +1,19 @@
 
 :- op(498, xfy, [or, and]).
-:- op(499, xfy, =>).
+:- op(499, xfy, implies).
+
+debug.
 
 :- initialization(main).
 main :- 
     load_kb('kb_lab.txt'),
-    inputs(Inputs),  
+    inputs(Inputs),
+    (debug, show_curves; true),  
     repeat,
     maplist(ask_rating, Inputs), nl,
 
     findall(rule(R), rule(R), Rules),
-    maplist(eval, Rules, Results),
-    writeln("Evaluation of each goal predicate:"),
-    maplist(writeln, Results),
+    maplist(eval, Rules, _),
 
     output(Goal, MinValue, MaxValue, Precision),
     find_centroid(aggregate_degree, MinValue, MaxValue, Precision, Result),
@@ -48,7 +49,7 @@ range(Start, Step, Stop, [Start | Other]) :-
 
 aggregate_degree(X, Y) :- 
     output(Goal, _, _, _),  
-    findall(Predicate, rule(_ => Goal/Predicate), GoalPredicates),
+    findall(Predicate, rule(_ implies Goal/Predicate), GoalPredicates),
     maplist(capped_degree(Goal, X), GoalPredicates, Ys),
     max_list(Ys, Y).
 
@@ -57,26 +58,41 @@ capped_degree(Input, X, Predicate, Y) :-
     max_satisfaction(Input/Predicate, M),
     Y is min(Y0, M).
 
-eval(rule(Antecedents => Consequent), Result) :- 
-    eval_consequent(rule(Antecedents => Consequent), ConsequentResult),
+show_curves :- 
+    findall(definition(Input, Predicates, Left, Right), definition(Input, Predicates, Left, Right), Definitions),
+    writeln(Definitions),
+    maplist(show_curvers_for_def, Definitions).
+
+show_curvers_for_def(definition(Input, Predicates, Left, Right)) :-
+    maplist(show_curvers_for_pred(Input, Left, Right), Predicates).
+
+show_curvers_for_pred(Input, Left, Right, Predicate) :-
+    range(Left, 1, Right, Range),
+    maplist(degree(Input/Predicate), Range, Results),
+    write("Cuvrve for "), write(Input/Predicate), write(": "), writeln(Results).
+
+eval(rule(Antecedents implies Consequent), Result) :- 
+    eval_consequent(rule(Antecedents implies Consequent), ConsequentResult),
     assertz(max_satisfaction(Consequent, ConsequentResult)),
-    Result = [Consequent, ConsequentResult].
+    Result = [Consequent, ConsequentResult],
+    write("Rule: "), write(Antecedents implies Consequent), write(" Degree: "), writeln(ConsequentResult), nl.
 
-eval_consequent(rule(A => _), Result) :- eval_antecedent(A, Result).
+eval_consequent(rule(A implies _), Result) :- eval_antecedent(A, Result).
 
-eval_consequent(rule(A or B => _), Result) :-
+eval_consequent(rule(A or B implies _), Result) :-
     eval_antecedent(A, ResultA),
     eval_antecedent(B, ResultB),
     Result is max(ResultA, ResultB).
 
-eval_consequent(rule(A and B => _), Result) :- 
+eval_consequent(rule(A and B implies _), Result) :- 
     eval_antecedent(A, ResultA),
     eval_antecedent(B, ResultB),
     Result is min(ResultA, ResultB).
 
 eval_antecedent(Input/Predicate, Result) :-
     rating(Input, Rating),
-    degree(Input/Predicate, Rating, Result).
+    degree(Input/Predicate, Rating, Result),
+    write("Premise: "), write(Input/Predicate), write(" Degree: "), write(Result), nl.
 
 ask_rating(Input) :-
     write(Input), write(" > "),
@@ -97,6 +113,7 @@ lin(Dir, Left, Right, X, Y) :-
 :- dynamic output/1.
 :- dynamic degree/3.
 :- dynamic rule/1.
+:- dynamic definition/3.
 load_kb(Filename) :-
 	see(Filename),
     repeat,
