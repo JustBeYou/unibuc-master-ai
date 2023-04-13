5ab9818bdc1a35b1f8c1cabd0bf7fffc17f2bb4f
JustBeYou <mihailferaru2000@gmail.com>
2023-04-13 19:26:37 +0300

    work CV

--- computer_vision/CV-2023-Project1/src/board.py
+++ computer_vision/CV-2023-Project1/src/board.py
@@ -1,25 +1,28 @@
 import dataclasses
-from typing import Dict
+from typing import List
 
 EMPTY = -1
-ROWS = list(range(1, 15 + 1))
+SCORE_TRACK_START = -1
+BOARD_SIZE = 15
+N = BOARD_SIZE - 1
 COLUMNS = "ABCDEFGHIJKLMNO"
 
+DIAMONDS = {}
 
 @dataclasses.dataclass
 class Board:
     """
-    A mapping of rows x columns -> domino values.
+    A mapping of rows x columns (15x15) -> domino values.
     Rows from 1 to 15, columns from A to O and values from 0 to 6.
     """
-    __board: Dict[int, Dict[str, int]]
+    __board: List[List[int]]
+    player_one_score_track: int
+    player_two_score_track: int
 
     def __init__(self):
-        self.__board = {}
-        for row in ROWS:
-            self.__board[row] = {}
-            for column in COLUMNS:
-                self.__board[row][column] = EMPTY
+        self.__board = [[EMPTY for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
+        self.player_one_score_track = SCORE_TRACK_START
+        self.player_two_score_track = SCORE_TRACK_START
 
     def __getitem__(self, item):
         return self.__board[item]
